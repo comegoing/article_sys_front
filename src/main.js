072bea0dc9c958c5d41d5b81dafc962dc18a3251
comegoing <2294347244@qq.com>
2025-10-12 13:10:03 +0800

    2025-10-12-1-基本UI搭建

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import Antd from "ant-design-vue";
+import "ant-design-vue/dist/antd.css";
+import moment from 'moment'
+import 'moment/locale/zh-cn'
+moment.locale('zh-cn')
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.use(Antd);
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
